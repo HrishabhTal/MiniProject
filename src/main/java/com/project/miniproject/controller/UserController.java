9a610061f6b79840d6a737e4e63a812f38cbbe6d
Shinichi <rishabhatre@gmail.com>
2024-01-24 17:08:01 +0530

    Added test cases for user entity

--- src/main/java/com/project/miniproject/controller/UserController.java
+++ src/main/java/com/project/miniproject/controller/UserController.java
@@ -2,6 +2,7 @@ package com.project.miniproject.controller;
 
 import java.net.URI;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.project.miniproject.entity.User;
 import com.project.miniproject.service.UserService;
@@ -17,7 +17,6 @@ import com.project.miniproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
 
 import java.util.List;
 
@@ -30,39 +29,43 @@ public class UserController {
 
 	@Autowired
     private final UserService userService;
+
+	private static final String USERENDPOINT = "/users";;
 	
-	@GetMapping("/users")
-	public List<User> retrieveAllUsers() {
-		return userService.getAllUsers();
+	@GetMapping(USERENDPOINT)
+	public ResponseEntity<?> retrieveAllUsers() {
+		List<User> response = userService.getAllUsers();
+		return  ResponseEntity.ok().body(response);
 	}
 
 	
 	
-	@GetMapping("/users/{id}")
-	public EntityModel<User> retrieveUser(@PathVariable int id) {
-		User user = userService.findUser(id);
+	@GetMapping(USERENDPOINT + "/{id}")
+	public ResponseEntity<?> retrieveUser(@PathVariable int id) {
 		
-		EntityModel<User> entityModel = EntityModel.of(user);
-	
-		return entityModel;
+		User user = userService.findUser(id);
+
+		if(user.getId() == null){
+            String response = "User Not Found.";
+            return ResponseEntity.status(404).body(response);
+        }
+		
+		return ResponseEntity.ok().body(user);
 	}
 	
-	@DeleteMapping("/users/{id}")
-	public void deleteUser(@PathVariable int id) {
+	
+	@DeleteMapping(USERENDPOINT+"/{id}")
+	public ResponseEntity<?> deleteUser(@PathVariable int id) {
 		userService.deleteUser(id);
+		return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
 	}
 
-	@PostMapping("/users")
+	
+	@PostMapping(USERENDPOINT)
 	public ResponseEntity<User> createUser(@RequestBody User user) {
 		
-		User savedUser = userService.addUser(user);
-
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-						.path("/{id}")
-						.buildAndExpand(savedUser.getId())
-						.toUri();   
-		
-		return ResponseEntity.created(location).build();
+		User response = userService.addUser(user);
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
 	}
     
 }
---
+++ src/test/java/com/project/miniproject/entity/UserTest.java
@@ -0,0 +1,54 @@
+package com.project.miniproject.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+import java.util.Optional;
+
+import com.project.miniproject.repository.UserRepository;
+
+@SpringBootTest
+@DataJpaTest
+public class UserTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testCreateUser() {
+        
+        String name = "Rick";
+        User user = new User(null, name, null);
+        
+        userRepository.save(user);
+        
+        assertEquals(user.getName(),name);
+    }
+
+    @Test
+    public void testListUsers() {
+        
+        List<User> users = (List<User>) userRepository.findAll();
+        
+        assertThat(users.size()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testDeleteUser() {
+        
+        String name ="Rick";
+        User user = userRepository.save(new User(null,name,null));
+        Integer userId = user.getId();
+        
+        userRepository.delete(user);
+        Optional<User> deletedUser = userRepository.findById(userId);
+        
+        assertEquals(deletedUser,null);
+    }
+
+    
+}
